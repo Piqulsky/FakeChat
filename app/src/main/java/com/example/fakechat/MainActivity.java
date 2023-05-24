24d797ed7a35e1ef8c2ddb79cb91619b975f2730
Drummermichal <drummermichal@gmail.com>
2023-05-24 21:25:40 +0200

    Added sending messages

--- app/src/main/java/com/example/fakechat/MainActivity.java
+++ app/src/main/java/com/example/fakechat/MainActivity.java
@@ -5,11 +5,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<MessagesData> recyclerData;
+    private String receiver;
+    private Map<String, ArrayList<MessagesData>> appData;
     private RecyclerView recyclerView;
     private MessagesAdapter recyclerAdapter;
     @Override
@@ -17,11 +23,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        receiver = "Receiver";
+
+        appData = new HashMap<>();
+        appData.put("Receiver", createDataArray());
+
         recyclerView = findViewById(R.id.recyclerView);
-        recyclerData = createDataArray();
-        recyclerAdapter = new MessagesAdapter(recyclerData, this);
+        recyclerAdapter = new MessagesAdapter(appData.get(receiver), this);
         recyclerView.setAdapter(recyclerAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, true));
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(false);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        ImageView sendButton = findViewById(R.id.imageViewSend);
+        sendButton.setOnClickListener(view -> {
+            EditText editTextMessage = findViewById(R.id.editTextMessage);
+            if(!editTextMessage.getText().toString().equals(""))
+                MainActivity.this.addToRecyclerData(new MessagesData(MessagesData.LAYOUT_MESSAGE_SENT, editTextMessage.getText().toString()));
+            editTextMessage.setText("");
+        });
     }
 
     private ArrayList<MessagesData> createDataArray(){
@@ -29,10 +50,16 @@ public class MainActivity extends AppCompatActivity {
 
         list.add(new MessagesData(MessagesData.LAYOUT_MESSAGE_RECEIVED, "This is message received", R.drawable.ic_launcher_foreground));
         list.add(new MessagesData(MessagesData.LAYOUT_MESSAGE_SENT, "This is message sent"));
-        list.add(new MessagesData(MessagesData.LAYOUT_MESSAGE_RECEIVED, "This is message received that is definetely much more longer and should be splitted", R.drawable.ic_launcher_foreground));
+        list.add(new MessagesData(MessagesData.LAYOUT_MESSAGE_RECEIVED, "This is message received that is definitely much more longer and should be split", R.drawable.ic_launcher_foreground));
         list.add(new MessagesData(MessagesData.LAYOUT_MESSAGE_SENT, "This is message sent"));
-        list.add(new MessagesData(MessagesData.LAYOUT_MESSAGE_SENT, "This is message sent that is definetely much more longer and should be splitted"));
+        list.add(new MessagesData(MessagesData.LAYOUT_MESSAGE_SENT, "This is message sent that is definitely much more longer and should be split"));
 
         return list;
+    }
+
+    public void addToRecyclerData(MessagesData messagesData){
+        appData.get(receiver).add(messagesData);
+        recyclerView.scrollToPosition(recyclerAdapter.getItemCount() - 1);
+        recyclerAdapter.notifyDataSetChanged();
     }
 }
